da94d1f267b8b4be04e380d6fd31fb232a738c1c
Marcela Mera <marcela.mera@stvincent.edu>
2025-07-09 14:04:39 -0400

    Update hello.cpp

--- Module1_HelloWorld/hello.cpp
+++ Module1_HelloWorld/hello.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    // TODO: Print your full name and your favorite number on separate lines
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
