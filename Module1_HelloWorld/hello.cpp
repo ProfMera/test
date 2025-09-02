d1f2bed4361d3574294dcb80edc109fb97e7a0e8
Marcela Mera <marcela.mera@stvincent.edu>
2025-09-02 17:06:11 +0000

    this is my first commit

--- Module1_HelloWorld/hello.cpp
+++ Module1_HelloWorld/hello.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     // TODO: Print your full name and your favorite number on separate lines
     cout << "Hello, World!" << endl;
+    cout << "Marcela" << endl;
     cout << "class";
 
     return 0;
